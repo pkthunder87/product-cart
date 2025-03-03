e891dcd2ef5f2b3d00a9acd2c34b767c5bb16abd
pkthunder87 <pkthunder87@gmail.com>
2025-03-02 16:35:23 -0800

    Feat: Add Attribution component

---
+++ src/components/Attribution.jsx
@@ -0,0 +1,23 @@
+function Attribution() {
+  return (
+    <div className="attribution">
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.frontendmentor.io/challenges/product-list-with-cart-5MmqLVAp_d"
+      >
+        Frontend Mentor
+      </a>
+      . Coded by{" "}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://github.com/pkthunder87/product-cart"
+      >
+        PkThunder.
+      </a>
+    </div>
+  );
+}
+
+export default Attribution;
